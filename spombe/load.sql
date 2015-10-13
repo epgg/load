22953817493d27a1a43494da513b30814b18827c
Daofeng Li <lidaof@gmail.com>
2015-10-13 18:02:57 -0500

    adding rn6,ce10,dm6

--- spombe/load.sql
+++ spombe/load.sql
@@ -5,6 +5,7 @@ kegg char(100) not null primary key,
 refgene char(100) not null
 );
 load data local infile 'kegg2refgene' into table kegg2refgene;
+load data local infile 'pombe_ID-usualNames' into table kegg2refgene;
 
 
 
@@ -15,8 +16,8 @@ start int unsigned not null,
 stop int unsigned not null,
 name varchar(100) not null
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-load data local infile 'pombase_gene_load' into table pombase_gene;
-load data local infile 'genename' into table pombase_gene;
+load data local infile 'pombe_usualNames' into table pombase_gene;
+load data local infile 'pombe_all_ID' into table pombase_gene;
 create index name on pombase_gene (name);
 
 
